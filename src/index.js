c732d9d44cd8ad753a26a1d1ed349882fd6cd039
Net-zen <prankster@myttk.ru>
2019-10-15 17:31:59 +0600

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  let zeros = [];
+  for(let i = 0; i < 9; i++){
+    for(let j = 0; j < 9; j++){
+      if(matrix[i][j] === 0){
+        zeros.push([i, j]);
+      }
+    }
+  }
+  for (let k = 0; k < zeros.length;) {
+    let n = Math.floor(zeros[k][0] / 3) * 3;
+    let m = Math.floor(zeros[k][1] / 3) * 3;
+    let num = matrix[zeros[k][0]][zeros[k][1]] + 1;
+    let solved = false;
+    while(!solved && num <= 9) {
+      let col = true, row = true, area = true, c = 0;  
+      while (col && row && (c < 9)) {
+        row = ((matrix[zeros[k][0]][c] === num)) ? false : true;
+        col = ((matrix[c][zeros[k][1]] === num)) ? false : true; 
+        c++;
+      }
+      if (row && col) {
+        for (let i = 0; i < 3; i++) {
+          for (let j = 0; j <  3; j++) {
+            area = (!area || (matrix[n + i][m + j] === num)) ? false : true;
+          }
+        }
+      }
+      if(col && row && area) {
+        solved = true;
+        matrix[zeros[k][0]][zeros[k][1]] = num;
+        k++;
+      } else {
+        num++;
+      }
+    }
+    if(!solved) {
+      matrix[zeros[k][0]][zeros[k][1]] = 0;
+      k--;
+    }
+  }
+  return matrix;
 }
